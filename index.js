fb6f961eabf023302bc4369be31fc035400092f9
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-05-25 20:27:52 +0100

    ft(add-sorting)

--- index.js
+++ index.js
@@ -11,9 +11,25 @@ const checkArrayItem = (arr1, arr2) => {
   return arr1.some((phoneNum) => arr2.indexOf(phoneNum) >= 0);
 };
 
+/**
+ * @desc gets users with similar contacts
+ */
 const getSimilarContacts = () => {
   let userDetail = {};
-  const similarContactList = directory.map((user, index) => {});
+  let prevArr;
+  const similarContactList = directory.forEach((user, index) => {
+    const currArr = user.contacts;
+    if (currArr === prevArr) {
+      console.log("we inside");
+      const contactExist = prevArr.some(
+        (contact) => currArr.indexOf(contact) >= 0
+      );
+      if (contactExist) {
+        console.log("we here", prevArr, currArr);
+      }
+    }
+    prevArr = currArr;
+  });
 };
 
-console.log(directory);
+console.log(getSimilarContacts());
