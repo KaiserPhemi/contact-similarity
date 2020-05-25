82e4a0a585cd1a4f170aa2683308aa192d064ae0
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-05-25 13:21:24 +0100

    chore(random_num)
    - add random number generators

--- src/phoneNumberGenerator.js
+++ src/phoneNumberGenerator.js
@@ -1,1 +1,24 @@
-const phoneNumberHeaders = ["07", "08", "09"];
+// constants
+const PHONE_NUMBER_PREFIX = ["070", "071", "080", "081", "090", "091"];
+const MIN_CONTACT_LIST = 700;
+const MAX_CONTACT_LIST = 900;
+
+// generate random numbers
+const phoneNum = () => Math.floor(10000000 + Math.random() * 90000000);
+const arrLength = () => {
+  const minList = Math.ceil(MIN_CONTACT_LIST);
+  const maxList = Math.floor(MAX_CONTACT_LIST);
+  return Math.floor(Math.random() * (maxList - minList + 1)) + minList;
+};
+
+// console.log(phoneNum);
+const getNumList = () => {
+  let num = [];
+  for (let i = 1; i <= 400; i++) {
+    num.push(phoneNum());
+  }
+  console.log(num);
+};
+
+getNumList();
+console.log(arrLength());
--- src/phoneNumbers.js
+++
@@ -1,3 +0,0 @@
-// 1. generate names
-// 2. generate phone numbers
-// 3.
